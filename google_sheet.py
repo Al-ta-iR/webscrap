f3d111890edbd34e5816692d3aa0bb193ee10fef
Al-ta-iR <artyom.oleynik@gmail.com>
2023-05-15 15:31:52 +0200

    google sheet new works format

--- google_sheet.py
+++ google_sheet.py
@@ -1,45 +1,108 @@
-import pygsheets
+import httplib2
+import json
+import os
+
+from apiclient import discovery
+from google.oauth2 import service_account
 
 
-def google_sheet_get_data(sheet_name, CLIENT_SECRETS_GOOGLE):
-    if CLIENT_SECRETS_GOOGLE == '':
-        gc = pygsheets.authorize()
-    else:
-        gc = pygsheets.authorize(CLIENT_SECRETS_GOOGLE)
+def google_sheet_get_data(GOOGLE_CREDENTIALS_VAL):
+    try:
+        scopes = [
+            "https://www.googleapis.com/auth/drive",
+            "https://www.googleapis.com/auth/drive.file",
+            "https://www.googleapis.com/auth/spreadsheets",
+        ]
+        secret = json.loads(GOOGLE_CREDENTIALS_VAL)
 
-    # Open spreadsheet and then worksheet
-    sh = gc.open(sheet_name)
-    wks = sh.sheet1
-    values = wks.get_all_values()
-    is_parse = False
-    urls_data = []
-    for row in values:
-        if not is_parse and "№" not in row and "Result" not in row:
-            continue
-        if not is_parse and "№" in row and "Result" in row:
-            is_parse = True
-            continue
-        if "===END===" in row:
-            is_parse = False
-            break
-        elif is_parse:
-            is_needed_item = False
-            for item in row:
-                if item != '' and ('http' in item or 'www.' in item):
-                    urls_data.append(item)
-                    is_needed_item = True
-                elif is_needed_item:
-                    item = item.split('►')
-                    # for keys in item:
-                    urls_data.append(item)
-                    
-                    is_needed_item = False
-#         value = tuple(map(str, value.split('►')))
+        spreadsheet_id = "1G2nXkcyPnGvFHOZnZAG_BJVdiYwjyNxdbUT51U5Acuw"
+        # range_name = 'WebScrap!A1:L50'
 
-    return urls_data
+        credentials = service_account.Credentials.from_service_account_info(
+            secret, scopes=scopes
+        )
+        service = discovery.build("sheets", "v4", credentials=credentials)
+
+        result = (
+            service.spreadsheets()
+            .values()
+            .get(spreadsheetId=spreadsheet_id, range="WebScrap")
+            .execute()
+        )
+        rows = result.get("values", [])
+
+        is_parse = False
+        urls_data = []
+        for row in rows:
+            if not is_parse and "№" not in row and "Result" not in row:
+                continue
+            if not is_parse and "№" in row and "Result" in row:
+                is_parse = True
+                continue
+            if "===END===" in row:
+                is_parse = False
+                break
+            elif is_parse:
+                is_needed_item = False
+                for item in row:
+                    if item != "" and ("http" in item or "www." in item):
+                        urls_data.append(item)
+                        is_needed_item = True
+                    elif is_needed_item:
+                        item = item.split("►")
+                        # for keys in item:
+                        urls_data.append(item)
+
+                        is_needed_item = False
+        #         value = tuple(map(str, value.split('►')))
+        return urls_data
+
+    except OSError as e:
+        print(e)
+        return False
 
 
+# import pygsheets
 
-if __name__ == '__main__':
-    urls_data = google_sheet_get_data('OSINT', '')
-    print(urls_data)
+
+# def google_sheet_get_data(sheet_name, CLIENT_SECRETS_GOOGLE):
+#     if CLIENT_SECRETS_GOOGLE == '':
+#         gc = pygsheets.authorize()
+#     else:
+#         gc = pygsheets.authorize(CLIENT_SECRETS_GOOGLE)
+
+#     # Open spreadsheet and then worksheet
+#     sh = gc.open(sheet_name)
+#     wks = sh.sheet1
+#     values = wks.get_all_values()
+#     is_parse = False
+#     urls_data = []
+#     for row in values:
+#         if not is_parse and "№" not in row and "Result" not in row:
+#             continue
+#         if not is_parse and "№" in row and "Result" in row:
+#             is_parse = True
+#             continue
+#         if "===END===" in row:
+#             is_parse = False
+#             break
+#         elif is_parse:
+#             is_needed_item = False
+#             for item in row:
+#                 if item != '' and ('http' in item or 'www.' in item):
+#                     urls_data.append(item)
+#                     is_needed_item = True
+#                 elif is_needed_item:
+#                     item = item.split('►')
+#                     # for keys in item:
+#                     urls_data.append(item)
+
+#                     is_needed_item = False
+# #         value = tuple(map(str, value.split('►')))
+
+#     return urls_data
+
+
+# if __name__ == '__main__':
+#     urls_data = google_sheet_get_data('Os.. ... ..nt', '')
+#     print(urls_data)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from urllib.parse import unquote
+
 # import datetime as dt
 import os
 import platform
@@ -10,48 +11,53 @@ import concurrent.futures
 import time
 import urllib.parse
 from google_sheet import google_sheet_get_data
+
 # from search_data import get_hub_data
 
 
 email_flag = 0
 start_time = time.time()
-is_os_windows = platform.system() == 'Windows'
+is_os_windows = platform.system() == "Windows"
 
 if is_os_windows:
-    env_path = os.path.join('secrets.env')  # ◄local ▼
+    env_path = os.path.join("secrets.env")  # ◄local ▼
     load_dotenv(env_path)
-    EMAIL_SENDER = os.getenv('EMAIL_SENDER')
-    PASSWORD_EMAIL_SENDER = os.getenv('PASSWORD_EMAIL_SENDER')
-    EMAIL_RECIEVER = os.getenv('EMAIL_RECIEVER')
-    CLIENT_SECRETS_GOOGLE = os.getenv('CLIENT_SECRETS_GOOGLE')
+    EMAIL_SENDER = os.getenv("EMAIL_SENDER")
+    PASSWORD_EMAIL_SENDER = os.getenv("PASSWORD_EMAIL_SENDER")
+    EMAIL_RECIEVER = os.getenv("EMAIL_RECIEVER")
+    # CLIENT_SECRETS_GOOGLE = os.getenv('CLIENT_SECRETS_GOOGLE')
+    GOOGLE_CREDENTIALS_VAL = os.getenv("GOOGLE_CREDENTIALS_VAL")
     # SITE_DATA = os.getenv('SITE_DATA')
     # PAGE_ID_SITE_DATA = os.getenv('PAGE_ID_SITE_DATA')
-    RUTRACKER_LOGIN_USERNAME = os.getenv('RUTRACKER_LOGIN_USERNAME')
-    RUTRACKER_LOGIN_PASSWORD = os.getenv('RUTRACKER_LOGIN_PASSWORD')
+    RUTRACKER_LOGIN_USERNAME = os.getenv("RUTRACKER_LOGIN_USERNAME")
+    RUTRACKER_LOGIN_PASSWORD = os.getenv("RUTRACKER_LOGIN_PASSWORD")
 else:
-    EMAIL_SENDER = os.environ.get('EMAIL_SENDER')  # online ▼
-    PASSWORD_EMAIL_SENDER = os.environ.get('PASSWORD_EMAIL_SENDER')
-    EMAIL_RECIEVER = os.environ.get('EMAIL_RECIEVER')
-    CLIENT_SECRETS_GOOGLE = os.environ.get('CLIENT_SECRETS_GOOGLE')
+    EMAIL_SENDER = os.environ.get("EMAIL_SENDER")  # online ▼
+    PASSWORD_EMAIL_SENDER = os.environ.get("PASSWORD_EMAIL_SENDER")
+    EMAIL_RECIEVER = os.environ.get("EMAIL_RECIEVER")
+    # CLIENT_SECRETS_GOOGLE = os.environ.get('CLIENT_SECRETS_GOOGLE')
+    GOOGLE_CREDENTIALS_VAL = os.environ.get("GOOGLE_CREDENTIALS_VAL")
     # SITE_DATA = os.environ.get('SITE_DATA')
     # PAGE_ID_SITE_DATA = os.environ.get('PAGE_ID_SITE_DATA')
-    RUTRACKER_LOGIN_USERNAME = os.environ.get('RUTRACKER_LOGIN_USERNAME')
-    RUTRACKER_LOGIN_PASSWORD = os.environ.get('RUTRACKER_LOGIN_PASSWORD')
+    RUTRACKER_LOGIN_USERNAME = os.environ.get("RUTRACKER_LOGIN_USERNAME")
+    RUTRACKER_LOGIN_PASSWORD = os.environ.get("RUTRACKER_LOGIN_PASSWORD")
 
 
-def search_string(search_trigger, url, value, change_counter, current_site_data, is_equals):
+def search_string(
+    search_trigger, url, value, change_counter, current_site_data, is_equals
+):
     allert = ""
     start = value.find(search_trigger)
-    end = value.find(':')
-    flag_counter_string = value[start+len(search_trigger):end]
+    end = value.find(":")
+    flag_counter_string = value[start + len(search_trigger) : end]
     if not flag_counter_string.isnumeric():
-        allert += f'Flag {value} does not have number'
+        allert += f"Flag {value} does not have number"
         return allert
     flag_counter = int(flag_counter_string)
     flag_search = value.split(":", 1)[1]
     count = current_site_data.count(flag_search)
     if (count == flag_counter) is is_equals:
-        return '', change_counter
+        return "", change_counter
     else:
         change_counter += 1
         allert += f'>> [{url.encode()}]\n    - flag [{flag_search.encode()}] {("needed" if is_equals else "NOT needed")} {flag_counter} - found {count}\n'
@@ -59,112 +65,128 @@ def search_string(search_trigger, url, value, change_counter, current_site_data,
 
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win32; x32; rv:106.0) Gecko/20100101 Firefox/111.0',
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win32; x32; rv:106.0) Gecko/20100101 Firefox/111.0",
 }
 
 
 def check_url(url_data):
     url, flag = url_data
-    allert = ''
+    allert = ""
     change_counter = 0
-    if '▼' in flag[0]:
+    if "▼" in flag[0]:
         return allert, change_counter
-    
+
     try:
         redirect = True
-        if '▲' in flag[0]:
+        if "▲" in flag[0]:
             redirect = False
-        if 'rutracker' in url:
+        if "rutracker" in url:
             data = {
                 "redirect": url[28:].replace("/", ""),
                 "login_username": RUTRACKER_LOGIN_USERNAME,
                 "login_password": RUTRACKER_LOGIN_PASSWORD,
                 "login": "%C2%F5%EE%E4",
             }
-            response = requests.post("https://rutracker.org/forum/login.php", headers=headers, allow_redirects=redirect, data=data)
+            response = requests.post(
+                "https://rutracker.org/forum/login.php",
+                headers=headers,
+                allow_redirects=redirect,
+                data=data,
+            )
         else:
             response = requests.get(url, headers=headers, allow_redirects=redirect)
     except Exception as e:
-        allert += f'URL: {url} has problem: {e}\n'
+        allert += f"URL: {url} has problem: {e}\n"
         return allert, change_counter
-    
+
     status_code = response.status_code
     if status_code != 200:
-        if '◄' not in flag[0]:
-            allert += f'* {status_code}: {url}\n'
+        if "◄" not in flag[0]:
+            allert += f"* {status_code}: {url}\n"
         return allert, change_counter
-    
+
     current_site_data = str(response.text)
     for value in flag:
-        search_trigger = '∟∟'
-        if value[0] != '◄':
+        search_trigger = "∟∟"
+        if value[0] != "◄":
             if search_trigger in value:
-                result_search, change_counter = search_string(search_trigger, url, value, change_counter, current_site_data, True)
+                result_search, change_counter = search_string(
+                    search_trigger, url, value, change_counter, current_site_data, True
+                )
                 allert += result_search
                 continue
             if not bool(re.search(value, current_site_data)):
                 change_counter += 1
-                allert += f'>> [{url.encode()}]\n    - not found [{value.encode()}]\n'
+                allert += f">> [{url.encode()}]\n    - not found [{value.encode()}]\n"
         else:
             if search_trigger in value:
-                result_search, change_counter = search_string(search_trigger, url, value, change_counter, current_site_data, False)
+                result_search, change_counter = search_string(
+                    search_trigger, url, value, change_counter, current_site_data, False
+                )
                 allert += result_search
                 continue
             if re.search(value[1:], current_site_data):
                 change_counter += 1
-                allert += f'>> [{url.encode()}]\n    - found [{value[1:].encode()}]\n'
-    
+                allert += f">> [{url.encode()}]\n    - found [{value[1:].encode()}]\n"
+
     return allert, change_counter
 
 
 def check_data(urls_data):
-    allert = ''
+    allert = ""
     change_counter = 0
-    url_data = [(urllib.parse.unquote(urls_data[i]), urls_data[i+1]) for i in range(0, len(urls_data), 2)]
-    
+    url_data = [
+        (urllib.parse.unquote(urls_data[i]), urls_data[i + 1])
+        for i in range(0, len(urls_data), 2)
+    ]
+
     with concurrent.futures.ThreadPoolExecutor() as executor:
         results = executor.map(check_url, url_data)
-    
+
     for result in results:
         allert += result[0]
         change_counter += result[1]
-    
-    if allert != '':
-        if '*' in allert:
-            lines_message = allert.split('\n')
-            sorted_lines = sorted(lines_message, key=lambda x: x.startswith('*'))
-            allert = '\n'.join(sorted_lines)
-        message = 'Changes:\n\n' + allert + '\n'
+
+    if allert != "":
+        if "*" in allert:
+            lines_message = allert.split("\n")
+            sorted_lines = sorted(lines_message, key=lambda x: x.startswith("*"))
+            allert = "\n".join(sorted_lines)
+        message = "Changes:\n\n" + allert + "\n"
         message_router(message, change_counter)
 
 
-def send_mail(
-    subject,
-    text
-):
+def send_mail(subject, text):
     try:
-        message = 'Subject: {}\n\n{}'.format(subject, f'{text}')
-        server = smtplib.SMTP_SSL('mail.inbox.lv', 465)
+        message = "Subject: {}\n\n{}".format(subject, f"{text}")
+        server = smtplib.SMTP_SSL("mail.inbox.lv", 465)
         server.login(EMAIL_SENDER, PASSWORD_EMAIL_SENDER)
         server.sendmail(EMAIL_SENDER, EMAIL_RECIEVER, message)
         server.quit()
     except Exception as e:
         if is_os_windows:
-            print(f'Email did not send: {e}')
-        server.sendmail(EMAIL_SENDER, EMAIL_RECIEVER, 'Subject: {}\n\n{}'.format('error', 'error'))
+            print(f"Email did not send: {e}")
+        server.sendmail(
+            EMAIL_SENDER, EMAIL_RECIEVER, "Subject: {}\n\n{}".format("error", "error")
+        )
     finally:
         pass
 
+
 def message_router(allert, change_counter):
     time_work = "\n--- %s seconds ---\n" % round((time.time() - start_time), 2)
-    message = (allert + time_work)
+    message = allert + time_work
     if is_os_windows:
         print()
         print(f'Изменений: {change_counter}{10*" "}\n{message}')
-        send_mail(f'Changes on monitored sites: {change_counter}', message)
+        send_mail(f"Changes on monitored sites: {change_counter}", message)
     else:
-        send_mail(f'Changes on monitored sites: {change_counter}', message)
+        send_mail(f"Changes on monitored sites: {change_counter}", message)
 
-if __name__ == '__main__':
-    urls_data = google_sheet_get_data('OSINT', CLIENT_SECRETS_GOOGLE)
-    check_data(urls_data)
+
+if __name__ == "__main__":
+    urls_data = google_sheet_get_data(GOOGLE_CREDENTIALS_VAL)
+    if urls_data is False:
+        message_router("Проблема с парсингом БД", 1)
+    else:
+        check_data(urls_data)
